2def03bbe7d6f3a10ccd956f46963112f3025f03
Trigex <trigex@protonmail.com>
2021-05-11 14:36:23 -0700

    setup prisma, got a basic ejs rendering setup

---
+++ prisma/migrations/20210511210030_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `isCool` BOOLEAN NOT NULL DEFAULT false,
+    `role` ENUM('USER', 'CONTRIBUTOR', 'ADMIN') NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User.name_unique`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `authorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- src/harukaze.js
+++ src/harukaze.js
@@ -1,8 +1,35 @@
 const config = require('../config.json');
 const koa = require('koa');
-const logger = require('koa-logger');
-const app = new koa();
+const koaLogger = require('koa-logger');
+const koaJson = require('koa-json');
+const koaRouter = require('koa-router');
+const koaEjs = require('koa-ejs');
+const koaStatic = require('koa-static');
+const { PrismaClient } = require('@prisma/client');
+const path = require('path');
 
-app.use(logger());
+const app = new koa();
+const router = new koaRouter();
+koaEjs(app, {
+    root: path.join(__dirname, '../res/views'),
+    layout: 'layout',
+    viewExt: 'ejs',
+    cache: false,
+    debug: false
+});
+
+// Routes
+router.get('/', async ctx => {
+    await ctx.render('index');
+});
+
+// Koa Middleware
+app.use(koaLogger());
+app.use(koaJson());
+app.use(koaStatic(path.join(__dirname, '../res/static')));
+app.use(router.routes()).use(router.allowedMethods());
+
+
+const prisma = new PrismaClient();
 
 app.listen(config.server.port);
