57c026170957a35ea90072cf95e006519df5d97f
= <termer@protonmail.com>
2021-07-08 20:24:59 -0400

    create moods and characters model

---
+++ src/models/moodchars.model.js
@@ -0,0 +1,135 @@
+const config = require('../../knexfile')
+const knex = require('knex')(config)
+
+/**
+ * Orders mood character results can be returned in
+ */
+const Order = {
+    /**
+     * Creation time, ascending
+     */
+    CREATED_ASC: 0,
+    /**
+     * Creation time, descending
+     */
+    CREATED_DESC: 1,
+    /**
+     * Name alphabetically, ascending
+     */
+    NAME_ASC: 2,
+    /**
+     * Name alphabetically, descending
+     */
+    NAME_DESC: 3
+}
+
+/* Utility functions */
+function charInfo() {
+    return knex('moodchars')
+        .select('moodchars.id')
+        .select(knex.ref('char_name').as('name'))
+        .select(knex.ref('char_default').as('default'))
+        .select(knex.ref('char_creator').as('creator'))
+        .select(knex.ref('user_username').as('creator_username'))
+        .select(knex.ref('char_created_on').as('created_on'))
+        .select(knex.raw(`(
+            SELECT COUNT(*)
+            FROM \`moods\`
+            WHERE \`mood_character\` = \`moodchars\`.\`id\`
+        ) AS \`moods\``))
+        .leftJoin('users', 'char_creator', 'users.id')
+}
+/**
+ * @param {Array<Object>} rows 
+ */
+function processCharInfoRows(rows) {
+    for(row of rows) {
+        row.created_on = new Date(row.created_on)
+    }
+    return rows
+}
+/**
+ * @param {number} order 
+ * @returns {string}
+ */
+function orderBy(order) {
+    switch(order) {
+    case Order.CREATED_DESC:
+        return '`moodchars`.`char_created_on` DESC'
+    case Order.TITLE_ASC:
+        return '`moodchars`.`char_name` ASC'
+    case Order.TITLE_DESC:
+        return '`moodchars`.`char_name` DESC'
+    default:
+        return '`moodchars`.`char_created_on` ASC'
+    }
+}
+
+/**
+ * Creates a new character
+ * @param {string} name The name
+ * @param {number} creator The creator's ID
+ */
+async function createCharacter(name, creator) {
+    return await knex('moodchars')
+        .insert({
+            char_name: name,
+            char_creator: creator
+        })
+}
+
+/**
+ * Fetches the character's info with the specified ID
+ * @param {number} id The ID
+ * @return {Array<Object>} An array with the row containing the character's info or an empty array if none exists
+ */
+async function fetchCharacterInfoById(id) {
+    return processCharInfoRows(
+        await charInfo()
+            .where('moodchars.id', id)
+    )
+}
+
+/**
+ * Fetches all characters' info
+ * @param {number} offset The offset to return results
+ * @param {number} limit The amount of results to return
+ * @param {number} order The order of results to return
+ * @return {Array<Object>} All characters' info
+ */
+async function fetchCharacterInfos(offset, limit, order) {
+    return processCharInfoRows(
+        await charInfo()
+            .offset(offset)
+            .limit(limit)
+            .orderByRaw(orderBy(order))
+    )
+}
+
+/**
+ * Fetches the amount of characters
+ * @returns {number} The amount of characters
+ */
+async function fetchCharactersCount() {
+    return (await knex('moodchars').count('*', { as: 'count' }))[0].count
+}
+
+/**
+ * Deletes the character with the specified ID
+ * @param {number} id The character ID
+ */
+async function deleteCharacterById(id) {
+    return await knex('moodchars')
+        .del()
+        .where('moodchars.id', id)
+}
+
+/* Export functions */
+module.exports.createCharacter = createCharacter
+module.exports.fetchCharacterInfoById = fetchCharacterInfoById
+module.exports.fetchCharacterInfos = fetchCharacterInfos
+module.exports.fetchCharactersCount = fetchCharactersCount
+module.exports.deleteCharacterById = deleteCharacterById
+
+/* Export values */
+module.exports.Order = Order
---
+++ src/models/moods.model.js
@@ -0,0 +1,165 @@
+const config = require('../../knexfile')
+const knex = require('knex')(config)
+
+/**
+ * Orders mood results can be returned in
+ */
+const Order = {
+    /**
+     * Creation time, ascending
+     */
+    CREATED_ASC: 0,
+    /**
+     * Creation time, descending
+     */
+    CREATED_DESC: 1,
+    /**
+     * Name alphabetically, ascending
+     */
+    NAME_ASC: 2,
+    /**
+     * Name alphabetically, descending
+     */
+    NAME_DESC: 3
+}
+
+/* Utility functions */
+function moodInfo() {
+    return knex('moods')
+        .select('moods.id')
+        .select(knex.ref('mood_name').as('name'))
+        .select(knex.ref('mood_key').as('key'))
+        .select(knex.ref('mood_character').as('character'))
+        .select(knex.ref('char_name').as('character_name'))
+        .select(knex.ref('mood_creator').as('creator'))
+        .select(knex.ref('user_username').as('creator_username'))
+        .select(knex.ref('mood_created_on').as('created_on'))
+        .leftJoin('moodchars', 'mood_character', 'moodchars.id')
+        .leftJoin('users', 'mood_creator', 'users.id')
+}
+/**
+ * @param {Array<Object>} rows 
+ */
+function processMoodInfoRows(rows) {
+    for(row of rows) {
+        row.created_on = new Date(row.created_on)
+    }
+    return rows
+}
+/**
+ * @param {number} order 
+ * @returns {string}
+ */
+function orderBy(order) {
+    switch(order) {
+    case Order.CREATED_DESC:
+        return '`moods`.`mood_created_on` DESC'
+    case Order.TITLE_ASC:
+        return '`moods`.`mood_name` ASC'
+    case Order.TITLE_DESC:
+        return '`moods`.`mood_name` DESC'
+    default:
+        return '`moods`.`mood_created_on` ASC'
+    }
+}
+
+/**
+ * Creates a new mood entry
+ * @param {string} name The name
+ * @param {string} key The key of the underlying file on disk
+ * @param {number} creator The creator's ID
+ */
+async function createMood(name, key, creator) {
+    return await knex('moods')
+        .insert({
+            mood_name: name,
+            mood_key: key,
+            mood_creator: creator
+        })
+}
+
+/**
+ * Fetches the mood's info with the specified ID
+ * @param {number} id The ID
+ * @return {Array<Object>} An array with the row containing the mood's info or an empty array if none exists
+ */
+async function fetchMoodInfoById(id) {
+    return processMoodInfoRows(
+        await moodInfo()
+            .where('moods.id', id)
+    )
+}
+
+/**
+ * Fetches all moods' info
+ * @param {number} offset The offset to return results
+ * @param {number} limit The amount of results to return
+ * @param {number} order The order of results to return
+ * @return {Array<Object>} All moods' info
+ */
+async function fetchMoodInfos(offset, limit, order) {
+    return processMoodInfoRows(
+        await moodInfo()
+            .offset(offset)
+            .limit(limit)
+            .orderByRaw(orderBy(order))
+    )
+}
+
+/**
+ * Fetches all moods' info with the specified character
+ * @param {number} character The character ID
+ * @param {number} offset The offset to return results
+ * @param {number} limit The amount of results to return
+ * @param {number} order The order of results to return
+ * @return {Array<Object>} All moods' info
+ */
+ async function fetchMoodInfosByCharacter(character, offset, limit, order) {
+    return processMoodInfoRows(
+        await moodInfo()
+            .where('mood_character', character)
+            .offset(offset)
+            .limit(limit)
+            .orderByRaw(orderBy(order))
+    )
+}
+
+/**
+ * Fetches the amount of moods
+ * @returns {number} The amount of moods
+ */
+async function fetchMoodsCount() {
+    return (await knex('moods').count('*', { as: 'count' }))[0].count
+}
+
+/**
+ * Deletes the mood with the specified ID
+ * @param {number} id The mood ID
+ */
+async function deleteMoodById(id) {
+    return await knex('moods')
+        .del()
+        .where('moods.id', id)
+}
+
+/**
+ * Deletes all moods with the specified character
+ * @param {number} character The character ID
+ */
+async function deleteMoodsByCharacter(character) {
+    return await knex('moods')
+        .del()
+        .where('mood_character', character)
+}
+
+/* Export functions */
+module.exports.createMood = createMood
+module.exports.fetchMoodInfoById = fetchMoodInfoById
+module.exports.fetchMoodInfos = fetchMoodInfos
+module.exports.fetchMoodInfosByCharacter = fetchMoodInfosByCharacter
+module.exports.fetchMoodsCount = fetchMoodsCount
+module.exports.deleteMoodById = deleteMoodById
+module.exports.deleteMoodsByCharacter = deleteMoodsByCharacter
+
+/* Export values */
+module.exports.Order = Order
