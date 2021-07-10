528b004820e56a52588181da26ec5adf72c71930
= <termer@protonmail.com>
2021-07-10 16:42:51 -0400

    make comment reply form collapseable

--- res/static/js/blog.js
+++ res/static/js/blog.js
@@ -5,20 +5,43 @@ const moods = document.getElementsByClassName('mood-chooser-moods')
 for(i in moodChoosers) {
     let idx = i
     let chooser = moodChoosers[i]
-    console.log(chooser)
     let opener = moodOpeners[i]
-    console.log(opener)
     let moodsElem = moods[i]
-    console.log(moodsElem.style.display)
 
-    moodsElem.style.display = 'none'
-    opener.onclick = function(e) {
-        if(moodsElem.style.display == 'block') {
-            opener.innerText = 'Moods ⯈'
-            moodsElem.style.display = 'none'
-        } else {
-            opener.innerText = 'Moods ⯆'
-            moodsElem.style.display = 'block'
+    if(moodsElem && moodsElem.style) {
+        moodsElem.style.display = 'none'
+        opener.onclick = function(e) {
+            if(moodsElem.style.display == 'block') {
+                opener.innerText = 'Moods ⯈'
+                moodsElem.style.display = 'none'
+            } else {
+                opener.innerText = 'Moods ⯆'
+                moodsElem.style.display = 'block'
+            }
+        }
+    }
+}
+
+const replyBoxes = document.getElementsByClassName('comment-reply-box')
+const boxOpeners = document.getElementsByClassName('comment-reply-open')
+const forms = document.getElementsByClassName('comment-reply-form')
+
+for(i in replyBoxes) {
+    let idx = i
+    let box = replyBoxes[i]
+    let opener = boxOpeners[i]
+    let formsElem = forms[i]
+
+    if(formsElem && formsElem.style) {
+        formsElem.style.display = 'none'
+        opener.onclick = function(e) {
+            if(formsElem.style.display == 'block') {
+                opener.innerText = 'Reply ⯈'
+                formsElem.style.display = 'none'
+            } else {
+                opener.innerText = 'Reply ⯆'
+                formsElem.style.display = 'block'
+            }
         }
     }
 }
