ff9bf4f45d98e3de0ff84ffa2fd4686c0c1890ba
= <termer@protonmail.com>
2021-09-04 17:24:05 -0400

    ignore cached scaled images from watch locations

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pm2 start src/harukaze.js --watch --ignore-watch "node_modules/ media/ media/avatars media/thumbnails media/moods .git config.json *.log" --attach --no-daemon
+pm2 start src/harukaze.js --watch --ignore-watch "node_modules/ media/ media/avatars media/thumbnails media/moods media/scaled .git config.json *.log" --attach --no-daemon
